25ebb63f79e486049d53cbe69b65bbe23255013c
fairoosa <fairoosa02112000@gmail.com>
2023-03-09 20:56:48 +0530

    scrapper updated

--- src/Dynamic_News_Portal/admin_management/views.py
+++ src/Dynamic_News_Portal/admin_management/views.py
@@ -9,26 +9,28 @@ class HomePage(generic.TemplateView):
     template_name = "home.html"
 
     def post(self, request, *args, **kwagrs):
-        url = self.request.POST['np']
-        duration = self.request.POST['du'] 
+        url = self.request.POST['np'].strip()
+        html_tag = self.request.POST['tag'].strip()
+        class_name = self.request.POST['class'].strip()
 
-        print("news poratl: ", url, "duration: ", duration)
+        print("news poratl: ", url, "html_tag: ", html_tag, "class_name: ", class_name)
 
         response = requests.get(url)
-        print("response: ", response)
+        # print("response: ", response)
 
         html_content = response.content
-        print("html content: ", html_content)
+        # print("html content: ", html_content)
 
         soup = BeautifulSoup(html_content, "html.parser") 
-        print("SOUP: ", soup)
+        # print("SOUP: ", soup)
         
-        title1 = soup.find('h1', class_='title').text
+        title1 = soup.find(html_tag, class_ = class_name).text
         print('Title:', title1)
 
-        title2 = soup.find('h3', class_='sub-title').text
-        print('Sub-Title:',title2)
+        # title2 = soup.find('h3').text
+        # print('Sub-Title:',title2)
 
-        contents = soup.find('div', id='content-body-65035597').text
-        print('Content :', contents)
+
+        # contents = soup.find('div', id='content-body-65035597').text
+        # print('Content :', contents)
         return redirect(reverse_lazy("home"))
