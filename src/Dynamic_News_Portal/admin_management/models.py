8b5dc8bbbf1668e888b7d7803602dcae76087071
fairoosa <fairoosa02112000@gmail.com>
2023-03-11 22:35:20 +0530

    Database Integeration

--- src/Dynamic_News_Portal/admin_management/models.py
+++ src/Dynamic_News_Portal/admin_management/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class NewsPortal(models.Model):
+    url = models.URLField(max_length = 200)
+    html_tag = models.CharField(max_length = 25)
+    class_name = models.CharField(max_length = 100)
+    date = models.DateTimeField(default = datetime.now())
+
+
+    def __str__(self):
+        return self.url
+
--- src/Dynamic_News_Portal/admin_management/views.py
+++ src/Dynamic_News_Portal/admin_management/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
 from bs4 import BeautifulSoup
 import requests
+from .models import NewsPortal
+
 
 class HomePage(generic.TemplateView):
     template_name = "home.html"
@@ -13,22 +15,23 @@ class HomePage(generic.TemplateView):
         html_tag = self.request.POST['tag'].strip()
         class_name = self.request.POST['class'].strip()
 
+        NewsPortal.objects.create(url=url, html_tag=html_tag, class_name=class_name)
         print("news poratl: ", url, "html_tag: ", html_tag, "class_name: ", class_name)
 
-        response = requests.get(url)
-        # print("response: ", response)
+        # response = requests.get(url)
+        # # print("response: ", response)
 
-        html_content = response.content
-        # print("html content: ", html_content)
+        # html_content = response.content
+        # # print("html content: ", html_content)
 
-        soup = BeautifulSoup(html_content, "html.parser") 
-        # print("SOUP: ", soup)
+        # soup = BeautifulSoup(html_content, "html.parser") 
+        # # print("SOUP: ", soup)
         
-        title1 = soup.find(html_tag, class_ = class_name).text
-        print('Title:', title1)
+        # title1 = soup.find(html_tag, class_ = class_name).text
+        # print('Title:', title1)
 
-        # title2 = soup.find('h3').text
-        # print('Sub-Title:',title2)
+        # # title2 = soup.find('h3').text
+        # # print('Sub-Title:',title2)
 
 
         # contents = soup.find('div', id='content-body-65035597').text
